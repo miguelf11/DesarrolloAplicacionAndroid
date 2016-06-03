21ac7aef70563a1be92109f8ffb3224181d45449
Miguel <miguelachof11@gmail.com>
2016-06-03 17:43:24 -0400

    listo botones de calcular,+,-

--- TipCalc/app/src/main/java/edu/galileo/android/tipcalc/MainActivity.java
+++ TipCalc/app/src/main/java/edu/galileo/android/tipcalc/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -62,9 +63,52 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.btnSubmit)
     public void handleClickSubmit(){
-        /*log.e sirve para mandar un mensaje */
-        Log.e(getLocalClassName(),"click en Submit");
         hideKeyboard();
+        String strInputTotal = inputBill.getText().toString().trim();
+        if(!strInputTotal.isEmpty()){
+            double total = Double.parseDouble(strInputTotal);
+            int tipPercentage = getTipPercentage();
+            double tip = total *(tipPercentage /100d);
+
+            String strTip = String.format(getString(R.string.global_message_tip),tip);
+            txtTip.setVisibility(View.VISIBLE);
+            txtTip.setText(strTip);
+
+        }
+    }
+
+
+    @OnClick(R.id.btnIncrease)
+    public void handleClickIncrease(){
+        hideKeyboard();
+        handleTipChange(TIP_STEP_CHANGE);
+    }
+
+    @OnClick(R.id.btnDecrease)
+    public void handleClickDecrease(){
+        hideKeyboard();
+        handleTipChange(- TIP_STEP_CHANGE);
+    }
+
+    private void handleTipChange(int change){
+        /* r  */
+        int tipPercentage = getTipPercentage();
+        tipPercentage += change;
+        if(tipPercentage > 0){
+            inputPercentage.setText(String.valueOf(tipPercentage));
+        }
+    }
+
+
+    public int getTipPercentage(){
+        int tipPercentage = DEFAULT_TIP_PERCENTAGE;
+        String strInputTipPercentage = inputPercentage.getText().toString().trim();
+        if (!strInputTipPercentage.isEmpty()){
+            tipPercentage = Integer.parseInt(strInputTipPercentage);
+        }else{
+            inputPercentage.setText(String.valueOf(tipPercentage));
+        }
+        return tipPercentage;
     }
 
     public void hideKeyboard(){
