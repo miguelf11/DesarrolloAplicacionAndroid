31fdac21ccb6aa41942f771ef9e92be024ad05c3
Miguel <miguelachof11@gmail.com>
2016-06-03 16:35:43 -0400

    video butterknife

--- TipCalc/app/src/main/java/edu/galileo/android/tipcalc/MainActivity.java
+++ TipCalc/app/src/main/java/edu/galileo/android/tipcalc/MainActivity.java
@@ -1,36 +1,85 @@
 package edu.galileo.android.tipcalc;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.inputBill)
+    EditText inputBill;
+    @BindView(R.id.btnSubmit)
+    Button btnSubmit;
+    @BindView(R.id.inputPercentage)
+    EditText inputPercentage;
+    @BindView(R.id.btnIncrease)
+    Button btnIncrease;
+    @BindView(R.id.btnDecrease)
+    Button btnDecrease;
+    @BindView(R.id.btnClear)
+    Button btnClear;
+    @BindView(R.id.txtTip)
+    TextView txtTip;
+
+    private final static int TIP_STEP_CHANGE = 1;
+    private final static int DEFAULT_TIP_PERCENTAGE = 10;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.action_about){
+        if (item.getItemId() == R.id.action_about) {
             about();
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void about(){
-        TipCalcApp app = (TipCalcApp)getApplication();
+
+    @OnClick(R.id.btnSubmit)
+    public void handleClickSubmit(){
+        /*log.e sirve para mandar un mensaje */
+        Log.e(getLocalClassName(),"click en Submit");
+        hideKeyboard();
+    }
+
+    public void hideKeyboard(){
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        try{
+            inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }catch (NullPointerException npe){
+            Log.e(getLocalClassName(),Log.getStackTraceString(npe));
+        }
+    }
+
+    private void about() {
+        TipCalcApp app = (TipCalcApp) getApplication();
         String strUrl = app.getAboutUrl();
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(strUrl));
