3967210afa9bdb0e481de0fcfdc15c9ec299bda7
Miguel <miguelachof11@gmail.com>
2016-06-09 02:29:29 -0400

    listo login butterknife , Log.e esta comentado porque no me lo reconoce android studio, pero no tiene mayor utilidad

--- AndroidChat/app/src/main/java/edu/galileo/android/androidchat/LoginActivity.java
+++ AndroidChat/app/src/main/java/edu/galileo/android/androidchat/LoginActivity.java
@@ -2,12 +2,31 @@ package edu.galileo.android.androidchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    @Bind(R.id.editTxtEmail)
+    EditText inputEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSignin)
+    public void handleSignin(){
+        //Log.e("AndroidChat",inputEmail.getText().toString());
+    }
+
+    @OnClick(R.id.btnSignup)
+    public void handleSignup(){
     }
 }
