b4e2c24c29d6f98e77eada3d3ad1355c7b1b5860
Miguel <miguelachof11@gmail.com>
2016-06-12 19:40:32 -0400

    MVP-Actividad

--- AndroidChat/app/src/main/java/edu/galileo/android/androidchat/contactlist/ui/ContactListActivity.java
+++ AndroidChat/app/src/main/java/edu/galileo/android/androidchat/contactlist/ui/ContactListActivity.java
@@ -1,15 +1,76 @@
 package edu.galileo.android.androidchat.contactlist.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import edu.galileo.android.androidchat.R;
+import edu.galileo.android.androidchat.contactlist.ContactListPresenter;
+import edu.galileo.android.androidchat.entities.User;
 
-public class ContactListActivity extends AppCompatActivity {
+public class ContactListActivity extends AppCompatActivity implements ContactListView {
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.recyclerViewContacts)
+    RecyclerView recyclerViewContacts;
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
+    private ContactListPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
+        ButterKnife.bind(this);
+
+        presenter.onCreate();
+        toolbar.setTitle(presenter.getCurrentUserEmail());
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        presenter.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        presenter.onPause();
+        super.onPause();
+    }
+
+    @OnClick(R.id.fab)
+    public void addContact(){
+
+    }
+
+
+    @Override
+    public void onContactAdded(User user) {
+
+    }
+
+    @Override
+    public void onContactChanged(User user) {
+
+    }
+
+    @Override
+    public void onContactRemoved(User user) {
+
     }
 }
