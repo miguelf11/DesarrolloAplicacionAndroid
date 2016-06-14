b657e4aeeb3d2973171a702ac6574aa74e9dcbeb
Miguel <miguelachof11@gmail.com>
2016-06-14 19:11:07 -0400

    AddContact Estructura MVP y Vista añadir interfaz AddContactView de los contactos

---
+++ AndroidChat/app/src/main/java/edu/galileo/android/androidchat/addcontact/AddContactView.java
@@ -0,0 +1,15 @@
+package edu.galileo.android.androidchat.addcontact;
+
+/**
+ * Created by Alex on 14-06-2016.
+ */
+public interface AddContactView {
+    void showInput();
+    void hideInput();
+    void showProgress();
+    void hideProgress();
+
+    void contactAdded();
+    void contactNotAdded();
+
+}
