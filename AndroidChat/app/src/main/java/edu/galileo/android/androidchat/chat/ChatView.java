b28a419fad5aaefbd42c7ed7749cfcf07c4f2e34
Miguel <miguelachof11@gmail.com>
2016-06-14 18:30:09 -0400

    Chat Estructura MVP: listo interfaz del ChatView

--- AndroidChat/app/src/main/java/edu/galileo/android/androidchat/chat/ChatView.java
+++ AndroidChat/app/src/main/java/edu/galileo/android/androidchat/chat/ChatView.java
@@ -1,7 +1,10 @@
 package edu.galileo.android.androidchat.chat;
 
+import edu.galileo.android.androidchat.entities.ChatMessage;
+
 /**
  * Created by Alex on 14-06-2016.
  */
 public interface ChatView {
+    void onMessageReceived(ChatMessage msg);
 }
