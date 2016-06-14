9e0953ddcaf7032a4d8d33899e0ead7a163ccf8a
Miguel <miguelachof11@gmail.com>
2016-06-14 19:15:52 -0400

    AddContact Estructura MVP y Vista: add Interactor

---
+++ AndroidChat/app/src/main/java/edu/galileo/android/androidchat/addcontact/AddContactInteractor.java
@@ -0,0 +1,12 @@
+package edu.galileo.android.androidchat.addcontact;
+
+/**
+ * Created by Alex on 14-06-2016.
+ */
+public interface AddContactInteractor {
+    //conecto el presentador con la lógica del negocio
+
+    void execute(String email);
+
+
+}
