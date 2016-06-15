4f72409a9d6da72816add0612872c25b703252ab
Miguel <miguelachof11@gmail.com>
2016-06-14 22:39:08 -0400

    AddContact Agregar Contacto: listo , pero tengo me da un NullPointerException cuando trato de agregar un contacto válido , me da error en el firebasehelper

---
+++ AndroidChat/app/src/main/java/edu/galileo/android/androidchat/addcontact/AddContactInteractorImpl.java
@@ -0,0 +1,17 @@
+package edu.galileo.android.androidchat.addcontact;
+
+/**
+ * Created by Alex on 14-06-2016.
+ */
+public class AddContactInteractorImpl implements AddContactInteractor {
+    AddContactRepository repository;
+
+    public AddContactInteractorImpl() {
+        repository = new AddContactRepositoryImpl();
+    }
+
+    @Override
+    public void execute(String email) {
+        repository.addContact(email);
+    }
+}
--- AndroidChat/app/src/main/java/edu/galileo/android/androidchat/addcontact/events/AddContactEvent.java
+++ AndroidChat/app/src/main/java/edu/galileo/android/androidchat/addcontact/events/AddContactEvent.java
@@ -4,4 +4,13 @@ package edu.galileo.android.androidchat.addcontact.events;
  * Created by Alex on 14-06-2016.
  */
 public class AddContactEvent {
+    boolean error = false;
+
+    public boolean isError() {
+        return error;
+    }
+
+    public void setError(boolean error) {
+        this.error = error;
+    }
 }
