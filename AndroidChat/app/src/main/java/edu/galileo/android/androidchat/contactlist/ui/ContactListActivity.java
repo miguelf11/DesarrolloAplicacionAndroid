29089e5b7cda0bc1558e58aec6bb06918d56ea5e
Miguel <miguelachof11@gmail.com>
2016-06-14 20:35:07 -0400

    AddContact Estructura MVP y Vista: cuando le di click a agregarcontacto llama a AddContactFragment

--- AndroidChat/app/src/main/java/edu/galileo/android/androidchat/contactlist/ui/ContactListActivity.java
+++ AndroidChat/app/src/main/java/edu/galileo/android/androidchat/contactlist/ui/ContactListActivity.java
@@ -18,6 +18,7 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import edu.galileo.android.androidchat.R;
+import edu.galileo.android.androidchat.addcontact.ui.AddContactFragment;
 import edu.galileo.android.androidchat.contactlist.ContactListPresenter;
 import edu.galileo.android.androidchat.contactlist.ContactListPresenterImpl;
 import edu.galileo.android.androidchat.contactlist.ui.adapters.ContactListAdapter;
@@ -110,7 +111,7 @@ public class ContactListActivity extends AppCompatActivity implements ContactLis
 
     @OnClick(R.id.fab)
     public void addContact() {
-
+        new AddContactFragment().show(getSupportFragmentManager(),getString(R.string.addcontact_message_title));
     }
 
 
