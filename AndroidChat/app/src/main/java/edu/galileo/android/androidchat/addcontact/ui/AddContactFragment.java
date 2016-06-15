fe510cecd565ccc211214251666d9ff632962371
Miguel <miguelachof11@gmail.com>
2016-06-14 20:32:57 -0400

    AddContact Estructura MVP y Vista: se crea addContactFragment

--- AndroidChat/app/src/main/java/edu/galileo/android/androidchat/addcontact/ui/AddContactFragment.java
+++ AndroidChat/app/src/main/java/edu/galileo/android/androidchat/addcontact/ui/AddContactFragment.java
@@ -1,30 +1,88 @@
 package edu.galileo.android.androidchat.addcontact.ui;
 
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import edu.galileo.android.androidchat.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AddContactFragment extends Fragment {
+public class AddContactFragment extends DialogFragment implements AddContactView {
 
+
+    @Bind(R.id.editTxtEmail)
+    EditText editTxtEmail;
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
 
     public AddContactFragment() {
         // Required empty public constructor
     }
 
 
+    @NonNull
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_contact, container, false);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        View view = getActivity().getLayoutInflater().inflate(R.layout.fragment_add_contact,null);
+        ButterKnife.bind(this, view);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(view);
+        AlertDialog dialog = builder.create();
+        return dialog;
     }
 
+
+
+    @Override
+    public void showInput() {
+        editTxtEmail.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideInput() {
+        editTxtEmail.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void contactAdded() {
+        Toast.makeText(getActivity(),R.string.addcontact_message_contactadded,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void contactNotAdded() {
+        editTxtEmail.setText("");
+        editTxtEmail.setError(getString(R.string.addcontact_error_message));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
