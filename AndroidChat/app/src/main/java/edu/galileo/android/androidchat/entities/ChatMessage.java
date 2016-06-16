d0c91468d62cd6d298ae2ccf71e2cd09e6afe449
Miguel <miguelachof11@gmail.com>
2016-06-16 18:46:40 -0400

    Chat Adapter: cambiamos la entidad ChatMessage

--- AndroidChat/app/src/main/java/edu/galileo/android/androidchat/entities/ChatMessage.java
+++ AndroidChat/app/src/main/java/edu/galileo/android/androidchat/entities/ChatMessage.java
@@ -1,7 +1,45 @@
 package edu.galileo.android.androidchat.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Created by Alex on 14-06-2016.
  */
+
+/*
+ ignora SentByMe porque NO es algo que firebase vaya a guardar
+ */
+
+@JsonIgnoreProperties({"sentByMe"})
 public class ChatMessage {
+    private String msg;
+    private String sender;
+    private boolean sentByMe;
+
+    public ChatMessage() {
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public boolean isSentByMe() {
+        return sentByMe;
+    }
+
+    public void setSentByMe(boolean sentByMe) {
+        this.sentByMe = sentByMe;
+    }
 }
