1a68a454ac1c00ec339202bef29ea44c2e8069b8
Miguel <miguelachof11@gmail.com>
2016-06-18 19:40:27 -0400

    Setup Gradle and Fabric: me falta ver lo del fabric key..

--- TwitterClient/app/src/main/java/edu/galileo/android/twitterclient/LoginActivity.java
+++ TwitterClient/app/src/main/java/edu/galileo/android/twitterclient/LoginActivity.java
@@ -2,12 +2,22 @@ package edu.galileo.android.twitterclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 public class LoginActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "8bKXyncTwvrXuozQgOyJZtzx8";
+    private static final String TWITTER_SECRET = "Ok9jbD7HSdNGNTsPkrQTDCBi1lyKZma7clj47hXKjkyp2xAE1i";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_login);
     }
 }
