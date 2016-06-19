05b3f912a800c2dab80571bea438a6fd74804402
Miguel <miguelachof11@gmail.com>
2016-06-19 02:18:08 -0400

    TwClientMain Layout

--- TwitterClient/app/src/main/java/edu/galileo/android/twitterclient/LoginActivity.java
+++ TwitterClient/app/src/main/java/edu/galileo/android/twitterclient/LoginActivity.java
@@ -1,16 +1,33 @@
 package edu.galileo.android.twitterclient;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+
 import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import edu.galileo.android.twitterclient.main.MainActivity;
 import io.fabric.sdk.android.Fabric;
 
 public class LoginActivity extends AppCompatActivity {
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
-    private static final String TWITTER_KEY = "8bKXyncTwvrXuozQgOyJZtzx8";
-    private static final String TWITTER_SECRET = "Ok9jbD7HSdNGNTsPkrQTDCBi1lyKZma7clj47hXKjkyp2xAE1i";
+    private static final String TWITTER_KEY = "8bK";
+    private static final String TWITTER_SECRET = "Ok9";
+    @Bind(R.id.twitterLoginButton)
+    TwitterLoginButton twitterLoginButton;
+    @Bind(R.id.container)
+    RelativeLayout container;
 
 
     @Override
@@ -19,5 +36,38 @@ public class LoginActivity extends AppCompatActivity {
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        if(Twitter.getSessionManager().getActiveSession() != null){
+            navigateToMainScreen();
+        }
+
+        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                navigateToMainScreen();
+            }
+
+            @Override
+            public void failure(TwitterException e) {
+                String msgError = String.format(getString(R.string.login_error_message),
+                        e.getLocalizedMessage());
+                Snackbar.make(container,msgError,Snackbar.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        twitterLoginButton.onActivityResult(requestCode,resultCode,data);
+    }
+
+    private void navigateToMainScreen() {
+        startActivity(new Intent(this, MainActivity.class));
+
+    }
+
+
 }
