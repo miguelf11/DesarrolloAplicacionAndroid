0d117828be4a4cd1840fa8379db49898ad0e2a35
Miguel <miguelachof11@gmail.com>
2016-07-03 19:33:47 -0400

    Main Activity

--- FacebookRecipes/app/src/main/java/edu/galileo/android/facebookrecipes/recipemain/ui/RecipeMainActivity.java
+++ FacebookRecipes/app/src/main/java/edu/galileo/android/facebookrecipes/recipemain/ui/RecipeMainActivity.java
@@ -1,15 +1,119 @@
 package edu.galileo.android.facebookrecipes.recipemain.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import edu.galileo.android.facebookrecipes.R;
+import edu.galileo.android.facebookrecipes.entities.Recipe;
+import edu.galileo.android.facebookrecipes.libs.base.ImageLoader;
+import edu.galileo.android.facebookrecipes.recipemain.RecipeMainPresenter;
 
-public class RecipeMainActivity extends AppCompatActivity {
+public class RecipeMainActivity extends AppCompatActivity implements RecipeMainView {
+
+    @Bind(R.id.imgRecipe)
+    ImageView imgRecipe;
+    @Bind(R.id.imgDismiss)
+    ImageButton imgDismiss;
+    @Bind(R.id.imgKeep)
+    ImageButton imgKeep;
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
+    @Bind(R.id.layoutContainer)
+    RelativeLayout layoutContainer;
+
+
+    private Recipe currentRecipe;
+    private ImageLoader imageLoader;
+    private RecipeMainPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_main);
+        ButterKnife.bind(this);
+        setUpInjection();
+        presenter.onCreate();
+        presenter.getNextRecipe();
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+    private void setUpInjection() {
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+
+    }
+
+    @Override
+    public void showUIElements() {
+        imgDismiss.setVisibility(View.VISIBLE);
+        imgKeep.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideUIElements() {
+        imgDismiss.setVisibility(View.GONE);
+        imgKeep.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void saveAnimation() {
+
+    }
+
+    @Override
+    public void dismissAnimation() {
+
+    }
+
+    @OnClick(R.id.imgKeep)
+    public void onKeep(){
+        if(currentRecipe != null){
+            presenter.saveRecipe(currentRecipe);
+        }
+    }
+
+    @OnClick(R.id.imgDismiss)
+    public void onDismiss(){
+        presenter.dismissRecipe();
+    }
+
+    @Override
+    public void onRecipeSaved() {
+        Snackbar.make(layoutContainer,R.string.recipemain_notice_saved,
+                Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void setRecipe(Recipe recipe) {
+        this.currentRecipe = recipe;
+        imageLoader.load(imgRecipe,recipe.getImageURL());
+    }
+
+    @Override
+    public void onGetRecipeError(String error) {
+        String msgError = String.format(getString(R.string.recipemain_error),error);
+        Snackbar.make(layoutContainer,error, Snackbar.LENGTH_SHORT).show();
     }
 }
