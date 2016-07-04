16998777f84dc426179d16cb5edca1fec94bb439
Miguel <miguelachof11@gmail.com>
2016-07-04 13:07:28 -0400

    List Repository

--- FacebookRecipes/app/src/main/java/edu/galileo/android/facebookrecipes/recipelist/RecipeListInteractorImpl.java
+++ FacebookRecipes/app/src/main/java/edu/galileo/android/facebookrecipes/recipelist/RecipeListInteractorImpl.java
@@ -1,7 +1,52 @@
 package edu.galileo.android.facebookrecipes.recipelist;
 
+import com.raizlabs.android.dbflow.list.FlowCursorList;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.Arrays;
+import java.util.List;
+
+import edu.galileo.android.facebookrecipes.entities.Recipe;
+import edu.galileo.android.facebookrecipes.recipelist.events.RecipeListEvent;
+
 /**
  * Created by Miguel on 04-07-2016.
  */
-public class RecipeListInteractorImpl {
+public class RecipeListInteractorImpl implements RecipeListRepository{
+
+    private EventBus eventBus;
+
+    @Override
+    public void getSavedRecipes() {
+        FlowCursorList<Recipe> storedRecipes = new FlowCursorList<Recipe>(false,Recipe.class);
+        post(RecipeListEvent.READ_EVENT,storedRecipes.getAll());
+        storedRecipes.close();
+    }
+
+    @Override
+    public void updateRecipe(Recipe recipe) {
+        recipe.update();
+        post();
+    }
+
+    @Override
+    public void removeRecipe(Recipe recipe) {
+        recipe.delete();
+        post(RecipeListEvent.DELETE_EVENT, Arrays.asList(recipe));
+    }
+
+    private void post(int type,List<Recipe> recipeList){
+        RecipeListEvent event = new RecipeListEvent();
+        event.setType(type);
+        event.setRecipeList(recipeList);
+        eventBus.post(event);
+    }
+
+    private void post(){
+        post(RecipeListEvent.UPDATE_EVENT,null);
+
+    }
+
+
 }
